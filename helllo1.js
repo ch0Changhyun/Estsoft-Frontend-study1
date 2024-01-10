084fed6fe3c3ff5cd795a79a817270c760c50f89
chc <311mikecho@gmail.com>
2024-01-10 15:00:54 +0900

    initial

--- helllo1.js
+++ helllo1.js
@@ -4,11 +4,23 @@
 // }
 // 나의함수();
 
-function 안녕(파라미터){
-    console.log(파라미터);
-    console.log('hello');
-    return 100;
+// function 안녕(파라미터){
+//     console.log(파라미터);
+//     console.log('hello');
+//     return 100;
+// }
+// let 아규먼트 = 1000;
+// 안녕(아규먼트);
+// console.log(안녕(아규먼트) + 안녕(아규먼트));
+
+// 함수 선언문
+function sum(x, y){
+    return x + y;
 }
-let 아규먼트 = 1000;
-안녕(아규먼트);
-console.log(안녕(아규먼트) + 안녕(아규먼트));
+
+// 함수 표현식
+let sumXY = function(x, y){
+    return x + y;
+};
+console.log(sum(10, 20));
+console.log(sumXY(10, 20));
