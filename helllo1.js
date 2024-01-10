b0555609bcc2eb1e7490815612e8884267c7bc63
chc <311mikecho@gmail.com>
2024-01-10 14:37:59 +0900

    init

---
+++ helllo1.js
@@ -0,0 +1,14 @@
+// console.log("hello");
+// function 나의함수(){
+//     console.log('함수의 결과')
+// }
+// 나의함수();
+
+function 안녕(파라미터){
+    console.log(파라미터);
+    console.log('hello');
+    return 100;
+}
+let 아규먼트 = 1000;
+안녕(아규먼트);
+console.log(안녕(아규먼트) + 안녕(아규먼트));
